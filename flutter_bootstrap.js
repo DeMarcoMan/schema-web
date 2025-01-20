a559cd56b2224bdeb3be876c12e68d827947de02
Marco van den Wildenberg <mvdwildenberg@gmail.com>
2025-01-20 17:09:28 +0100

    v1.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "464634849"
+    serviceWorkerVersion: "3870575278"
   }
 });
