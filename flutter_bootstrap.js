66646821c0fa045f95150d15bfe4f395f9562bcb
Marco van den Wildenberg <m.van.den.wildenberg@dalosy.com>
2025-02-18 21:15:18 +0100

    v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3965888010"
+    serviceWorkerVersion: "4123130278"
   }
 });
