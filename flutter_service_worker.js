342b06f267338c219058377205d96c1b68748c50
Marco van den Wildenberg <m.van.den.wildenberg@dalosy.com>
2025-02-18 19:18:29 +0100

    Better GUI

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,16 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "12d390af479a38b133c52d05b9a48f0b",
-"assets/AssetManifest.bin.json": "2d6fef3d6ebe2676455cbc54d3e066e4",
-"assets/AssetManifest.json": "8beffb2ee0e4c5020acb8fc6b96a162f",
+const RESOURCES = {"assets/AssetManifest.bin": "56fe024b9bd33f5d92e35b1d44645385",
+"assets/AssetManifest.bin.json": "a56d27167a3f3e817deed92e808ce455",
+"assets/AssetManifest.json": "d55cc36b88509dd2637b7e64d51b84fe",
+"assets/assets/images/hospice-elim.png": "ddda0c6cbfbd0153c7c653f8203dab57",
 "assets/assets/images/nachtmarathon.jpg": "64de1e9aee5545b6c9f56f18edd3b873",
 "assets/assets/images/qr.png": "5c702cf6cb39800539b5ef4ffa2fba07",
 "assets/assets/images/swimming-man.png": "8cdc62abf0bb39c3e03a7968b9502d9a",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/fonts/MaterialIcons-Regular.otf": "cffb147d01c2de52e075c56ab6969554",
-"assets/NOTICES": "f02154df9a8c60e29201b82c88ff41d5",
+"assets/fonts/MaterialIcons-Regular.otf": "9442214a109b3f3335a1683649a188e1",
+"assets/NOTICES": "c04349d13feb59f11e3dbe7c1e7daf78",
 "assets/packages/app_orchestration/assets/sounds/beep.wav": "2b6d49ea214857c14d394a6f809f1cbc",
 "assets/packages/app_orchestration/assets/sounds/beep_error.wav": "843d85b5d826505368a0ddb33faaa3ff",
 "assets/packages/app_orchestration/assets/sounds/beep_notice.wav": "baad61e0a8ad5fa925a9fa37981bf994",
@@ -30,12 +31,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "12d390af479a38b133c52d05b9a48f0b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "0e9e3ed31609101478bb6af7a1370b19",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "c864c4f04ff37ef7f3e86fbfa1475a2e",
+"flutter_bootstrap.js": "7b78aac13be23b8d64bbd92610b1091b",
 "icons/Icon-192.png": "79cbafa95284638b3188ed133bf9f1e8",
 "icons/Icon-512.png": "b73e8e4df9400654d07b3ca2a96f7943",
 "index.html": "01602ef386bcac16a2e76aff3f964147",
 "/": "01602ef386bcac16a2e76aff3f964147",
-"main.dart.js": "9d4d5b8db206a75669b17e4f81844fba",
+"main.dart.js": "13b6207e58b2c74e1f01ef40c8905a1b",
 "manifest.json": "90b2e3e06892726a07e13ad62cd79e03",
 "version.json": "26d48fe769e6684d5cc22aa12b6c0611"};
 // The application shell files that are downloaded before a service worker can
