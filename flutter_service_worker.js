e461a381905f3d346c3e5d21858e81bf6a48c8c7
Marco van den Wildenberg <m.van.den.wildenberg@dalosy.com>
2025-02-20 17:18:38 +0100

    v9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,12 +30,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "935b97333ec0baed7b9966e16d4d8a4f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "0e9e3ed31609101478bb6af7a1370b19",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "5fe965aef8438bb5dc330ffeb05dbdc0",
+"flutter_bootstrap.js": "a1d358dbebb5fdcb1c1497e45eed6e0a",
 "icons/Icon-192.png": "79cbafa95284638b3188ed133bf9f1e8",
 "icons/Icon-512.png": "b73e8e4df9400654d07b3ca2a96f7943",
 "index.html": "01602ef386bcac16a2e76aff3f964147",
 "/": "01602ef386bcac16a2e76aff3f964147",
-"main.dart.js": "df7e81f13acfb8f9fa57e24e053280e3",
+"main.dart.js": "f8eb5ab42cc849b07c9e1807d7ea9aed",
 "manifest.json": "33391a23a2a4be5ec446e7f287c96839",
 "version.json": "560c6991a708b1d3be6d92ec2226e247"};
 // The application shell files that are downloaded before a service worker can
